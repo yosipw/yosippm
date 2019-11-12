8ccc3cd877714375a8b876cf40bea447dd3457d3
yosipw <yosua.simaremare@gmail.com>
2019-11-12 16:30:20 +0800

    first commit

--- src/test/java/io/agileintelligence/ppmtool/PpmtoolApplicationTests.java
+++ src/test/java/io/agileintelligence/ppmtool/PpmtoolApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package io.agileintelligence.ppmtool;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class PpmtoolApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
